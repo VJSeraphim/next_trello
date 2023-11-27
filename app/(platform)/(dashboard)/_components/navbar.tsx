b98f388bafb5767c247f1df699fc5f07088309f3
VJS <minsung_96@naver.com>
2023-11-27 19:22:37 +0900

    Clerk Authentication + Navbar

---
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,52 @@
+import { Plus } from "lucide-react"
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs"
+
+import { Logo } from "@/components/logo"
+import { Button } from "@/components/ui/button"
+
+export const Navbar = () => {
+    return (
+        <nav className="fized z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
+            {/* Mobile Sidebar */}
+            <div className="flex items-center gap-x-4">
+                <div className="hidden md:flex">
+                    <Logo />
+                </div>
+                <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                    Create
+                </Button>
+                <Button size="sm" className='rounded-sm block md:hidden'>
+                    <Plus className="w-4 h-4" />
+                </Button>
+            </div>
+            <div className="flex gap-x-2 items-center ml-auto">
+                <OrganizationSwitcher 
+                    hidePersonal
+                    afterCreateOrganizationUrl="/organization/:id"
+                    afterLeaveOrganizationUrl="/select-org"
+                    afterSelectOrganizationUrl="/organization/:id"
+                    appearance={{
+                        elements:{
+                            rootBox: {
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center"
+                            }
+                        }
+                    }}
+                />
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements:{
+                            avatarBox: {
+                                width: 30,
+                                height: 30
+                            }
+                        }
+                    }}
+                />
+            </div>
+        </nav>
+    )
+}
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+const OrganizationIdPage = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default OrganizationIdPage
