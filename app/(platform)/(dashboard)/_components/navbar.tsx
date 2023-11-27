9871025f7c44254011f96e6d4a46c2976ff807d2
VJS <minsung_96@naver.com>
2023-11-27 19:22:49 +0900

    Clerk Authentication + Navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -12,10 +12,10 @@ export const Navbar = () => {
                 <div className="hidden md:flex">
                     <Logo />
                 </div>
-                <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
                     Create
                 </Button>
-                <Button size="sm" className='rounded-sm block md:hidden'>
+                <Button variant="primary" size="sm" className='rounded-sm block md:hidden'>
                     <Plus className="w-4 h-4" />
                 </Button>
             </div>
