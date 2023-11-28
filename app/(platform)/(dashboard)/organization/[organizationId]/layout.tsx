008f22ffba93bd45f04a22aa5a98921a95541b12
VJS <minsung_96@naver.com>
2023-11-28 19:49:23 +0900

    Sidebar Layouts

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,18 +1,16 @@
-const OrganizationLayout = ({
+import { OrgControl } from "./_components/org-control"
+
+const ORganizationIdLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
     return (
-        <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:w-screen-xl mx-auto">
-            <div className="flex gap-x-7">
-                <div className="w-64 shrink-0 hidden md:block">
-                    {/* Sidebar */}
-                </div>
-                {children}
-            </div>
-        </main>
+        <>
+            <OrgControl />
+            {children}
+        </>
     )
 }
 
-export default OrganizationLayout
+export default ORganizationIdLayout
