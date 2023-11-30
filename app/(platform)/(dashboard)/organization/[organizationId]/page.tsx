a627bd08e0a9516686ac3f92e5c419039fb4cb21
VJS <minsung_96@naver.com>
2023-11-30 18:58:32 +0900

    Database Searching

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,18 @@
-const OrganizationIdPage = () => {
+import { create } from "@/actions/create-board"
 
+const OrganizationIdPage = () => {
+    
     return (
         <div>
-            
+            <form>
+                <input
+                    id="title"
+                    name="title"
+                    required
+                    placeholder="Enter a board Title"
+                    className="border-black border"
+                />
+            </form>
         </div>
     )
 }
