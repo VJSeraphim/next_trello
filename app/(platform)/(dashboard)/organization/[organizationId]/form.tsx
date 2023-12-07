d9e749771572d49e20d779d188a13abe4a85f816
VJS <minsung_96@naver.com>
2023-12-08 00:31:07 +0900

    useformstate

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -2,10 +2,14 @@
 
 import { Button } from "@/components/ui/button"
 import { create } from "@/actions/create-board"
+import { useFormState } from "react-dom"
 
 export const Form = () => {
+    const initialState = { message: null, errors: {} }
+    const [state, dispatch] = useFormState(create, initialState)
+
     return (
-        <form action={create}>
+        <form action={dispatch}>
             <input
                 id="title"
                 name="title"
